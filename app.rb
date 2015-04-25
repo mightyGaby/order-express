f09e7927762a1023bd2609b47ad00254a0b4f10d
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2015-04-25 16:37:18 -0500

    sets up app structure and config.ru

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'bundler'
+Bundler.require()
+
+#========== * CONNECT TO DATABASE
+
+#========== * MODELS
+
+#========== * HELPER FUNCTIONS
+
+#========== * ROUTES
+
+#========== * API ROUTES
