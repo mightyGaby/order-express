c954c5c276969f7693b6819648fb6fa6f6b92ed5
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2015-04-25 19:34:55 -0500

    fixes table relations in restaurant database

--- migrations.sql
+++ migrations.sql
@@ -8,22 +8,24 @@ CREATE TABLE section (id SERIAL PRIMARY KEY);
 
 CREATE TABLE employee (id SERIAL PRIMARY KEY, section_id INTEGER REFERENCES section(id));
 
-CREATE TABLE table (
+CREATE TABLE tbl (
   id SERIAL PRIMARY KEY,
   section_id INTEGER REFERENCES section(id),
   employee_id INTEGER REFERENCES employee(id),
   active_status BOOLEAN
   );
 
-CREATE TABLE seat (id SERIAL PRIMARY KEY, table_id INTEGER REFERENCES table(id));
+CREATE TABLE seat (id SERIAL PRIMARY KEY, tbl_id INTEGER REFERENCES tbl(id));
 
 CREATE TABLE party (
   id SERIAL PRIMARY KEY,
   party_name VARCHAR(80),
   party_size INTEGER,
-  table_id INTEGER REFERENCES table(id),
+  tbl_id INTEGER REFERENCES tbl(id),
   paid_status BOOLEAN
   );
+
+CREATE TABLE restriction (id SERIAL PRIMARY KEY, description VARCHAR (255));
 
 CREATE TABLE guest (
   id SERIAL PRIMARY KEY,
@@ -33,13 +35,11 @@ CREATE TABLE guest (
   dietary_restriction INTEGER REFERENCES restriction(id)
 );
 
-CREATE TABLE restriction (id SERIAL PRIMARY KEY, description VARCHAR (255));
-
 CREATE TABLE request(
   id SERIAL PRIMARY KEY,
   item_id INTEGER REFERENCES menu_item(id),
   guest_id INTEGER REFERENCES guest(id),
-  order_id INTEGER REFERENCES order(id),
+  meal_id INTEGER REFERENCES meal(id),
   description VARCHAR(255)
 );
 
@@ -50,26 +50,23 @@ CREATE TABLE menu_item(
   restriction_id INTEGER REFERENCES restriction(id)
 );
 
-CREATE TABLE order(
+CREATE TABLE meal (
   id SERIAL PRIMARY KEY,
   guest_id INTEGER REFERENCES guest(id),
-  total_id INTEGER REFERENCES meal_total(id),
-  bill_id INTEGER REFERENCES bill(id),
+  total_id INTEGER REFERENCES meal_total(id)
 );
 
 CREATE TABLE meal_total(
   id SERIAL PRIMARY KEY,
   party_id INTEGER REFERENCES party(id),
-  table_id INTEGER REFERENCES table(id),
+  tbl_id INTEGER REFERENCES tbl(id)
 );
 
 CREATE TABLE bill(
   id SERIAL PRIMARY KEY,
   guest_id INTEGER REFERENCES guest(id),
   total_id INTEGER REFERENCES meal_total(id),
-  bill_id INTEGER REFERENCES bill(id),
-  order_id INTEGER REFERENCES order(id),
+  meal_id INTEGER REFERENCES meal(id),
   party_id INTEGER REFERENCES party(id),
-  table_id INTEGER REFERENCES table(id),
-  current_timestamp
+  tbl_id INTEGER REFERENCES tbl(id)
 );
